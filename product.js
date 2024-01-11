d2e23d40f1413ec1186c9c697a0406e83ad50cdf
Deepakkumardk123 <deepakofficialzking@gmail.com>
2024-01-11 19:35:06 +0530

    updated

---
+++ product.js
@@ -0,0 +1,20 @@
+var  sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+
+search.addEventListener("keyup",function()
+{
+    var enterdvalue
+})
